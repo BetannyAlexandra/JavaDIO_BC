a6db5e984e5dd962082d29395b8eaa003fcd8c8b
Bétanny <ainateb1972@gmail.com>
2024-05-29 17:07:44 -0300

    ExemplosDIO

--- Testes/ImprimirNumeros.java
+++
@@ -1,7 +0,0 @@
-public class ImprimirNumeros {
-    public static void main(String[] args) {
-        for (int i = 1; i <= 200; i++) {
-            System.out.println(i);
-        }
-    }
-}
--- Testes/print.java
+++
@@ -1,7 +0,0 @@
-public class print {
-    public static void main(String[] args) {
-       int b=9;
-            System.out.println(b);
-        
-    }
-}
